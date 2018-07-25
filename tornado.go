3cde37551fd384b0b5db6bd2ee7e6e1dd6ffd243
tomocy <tomocy.dev@gmail.com>
2018-07-26 01:05:15 +0900

    add command in tornado.cli

--- tornado.go
+++ tornado.go
@@ -1,10 +1,28 @@
 package tornado
 
+import clilib "github.com/mitchellh/cli"
+
+const (
+	appName = "tornado"
+	version = "1.0.0"
+)
+
 type Tornado interface {
 	Run() int
 }
 
 type tornado struct {
+	cli *clilib.CLI
+}
+
+func newTornado() *tornado {
+	cli := clilib.NewCLI(appName, version)
+	cli.Commands = map[string]clilib.CommandFactory{
+		"list": listCommandFacotry,
+	}
+	return &tornado{
+		cli: cli,
+	}
 }
 
 func (t tornado) Run() int {
--- tornado_test.go
+++ tornado_test.go
@@ -1,8 +1,45 @@
 package tornado
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestRunSuccessfully(t *testing.T) {
+func TestNewTornado(t *testing.T) {
+	given := newTornado()
+	thens := getNewTornadoTestCases()
+	for _, then := range thens {
+		_, have := given.cli.Commands[then.in]
+		if have != then.want {
+			t.Errorf("have %#v, but want %#v\n", have, then.want)
+			if then.want {
+				t.Errorf("add %#v command\n", then.in)
+			} else {
+				t.Errorf("remove %#v command\n", then.in)
+			}
+		}
+	}
+}
+
+func getNewTornadoTestCases() []struct {
+	in   string
+	want bool
+} {
+	return []struct {
+		in   string
+		want bool
+	}{
+		{
+			in:   "list",
+			want: true,
+		},
+		{
+			in:   "unknown",
+			want: false,
+		},
+	}
+}
+
+func TestTornadoRunSuccessfully(t *testing.T) {
 	given := new(tornado)
 	want := getExpectationAsRunningSuccessfully()
 	have := given.Run()
